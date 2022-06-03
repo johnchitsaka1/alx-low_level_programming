dee5a9554791aef2d2c01d9a510c428dd4a92cdc
johnchitsaka1 <tanakachitsaka1@gmail.com>
2022-06-03 05:51:42 -0700

    0 corrected

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -22,7 +22,9 @@ int main(void)
         else if (n<0)
 	{
                 printf("%d is negative\n",n);
-        }else{
+	}
+	else if (n==0)
+	{
                 printf("%d is zero",n);
         }
 	return (0);
