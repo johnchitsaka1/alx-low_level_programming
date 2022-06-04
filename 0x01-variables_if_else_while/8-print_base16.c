03d6ed579e833bdea3181ad2470c8c572b089f04
johnchitsaka1 <tanakachitsaka1@gmail.com>
2022-06-03 19:51:28 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main- prints all gex digits
+ *
+ *Return: 0 when executed ok
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 48; i <= 58; i++)
+	{
+		putchar(i);
+	}
+	for (c = 'A'; c <= 'D'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
