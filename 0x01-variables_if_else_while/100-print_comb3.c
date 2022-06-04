0144a8e87e5a11630a2725f248e89b3450dc6be5
johnchitsaka1 <tanakachitsaka1@gmail.com>
2022-06-03 20:23:47 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main- prints out posible combinations of two digits
+ *
+ *Return: 0 when executed ok
+ */
+in main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 49; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			putchar(',');
+		}
+	}
+	return (0);
+}
