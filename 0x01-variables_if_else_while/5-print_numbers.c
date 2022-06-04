e96de7e5a67720dbb6c80e1da01c1283b62908d1
johnchitsaka1 <tanakachitsaka1@gmail.com>
2022-06-03 19:20:57 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main- prints numbers from zero to 9
+ *
+ *Return: 0 if program executed ok
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		printf(i);
+		i++;
+	}
+	putchar('\n');
+}
