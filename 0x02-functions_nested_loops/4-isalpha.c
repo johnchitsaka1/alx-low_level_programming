95301b4c84ba8539b42da0511f53fb818c12c7b4
johnchitsaka1 <tanakachitsaka1@gmail.com>
2022-06-07 20:01:47 -0700

    1-4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-int _isalpha(int c);
+#include "main.h"
+
 /**
  *main - check the code
  *
@@ -17,24 +17,4 @@ int main(void)
 	putchar(r + '0');
 	putchar('\n');
 	return (0);
-}
-
-/**
- *_isalpha - tests input to see if it is part of the alphabet
- *@i-
- *Return: 1 when input is a letter , 0 otherwise
- */
-int _isalpha(int c)
-{
-	int i;
-
-	i = c;
-	if (((i > 64) && (i < 91)) || ((i > 96) && (i < 123)))
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
 }
