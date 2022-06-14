cc2b2195d5e54765f8b2e5baa0428793d280588b
johnchitsaka1 <tanakachitsaka1@gmail.com>
2022-06-13 18:00:56 -0700

    1

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -16,4 +16,11 @@ int _putchar(char c);
  */
 void reset_to_98(int *n);
 
+/**
+ *swap_int - swaps values of two integers
+ *@a: first value
+ *@b: second value
+ */
+void swap_int(int *a, int *b);
+
 #endif
