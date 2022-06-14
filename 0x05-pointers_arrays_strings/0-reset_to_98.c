f5e238cad9e87a9ac1941952197feba498271792
johnchitsaka1 <tanakachitsaka1@gmail.com>
2022-06-13 17:45:01 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+/**
+ *reset_to_98 - updates value to 98 via pointer
+ *@*n: inter to be passed
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ *_putchar - prints a single char to std out
+ *@c: character to print
+ *
+ *Return:On success.
+ *On errror, -1 is returned, and erno is set approprietly
+ */
+int _putchar(char c);
+
+/**
+ *reset_to_98 - resets an int value to 98 via a pointer
+ *@*n:pointer o change int value
+ */
+void reset_to_98(int *n);
+
+#endif
